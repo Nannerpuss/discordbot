ee6ff00b190ee47647373c7fb2f99cb58e1381f1
Nannerpuss <vintetta@gmail.com>
2021-04-24 14:51:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerGroup('tarot', 'Tarot');
+bot.registry.registerGroup('lore', 'Lore');
+bot.registry.registerGroup('shuffle', 'Shuffle');
+bot.registry.registerGroup('chargen', 'Character Generation');
+bot.registry.registerGroup('gems', 'Gems');
+bot.registry.registerGroup('information', 'Information');
+bot.registry.registerGroup('weapons', 'Weapons');
+bot.registry.registerGroup('inspire', 'Inspire');
+bot.registry.registerGroup('fireball', 'Fireball');
+bot.registry.registerGroup('goodluck', 'GoodLuck');
+bot.registry.registerGroup('wtfscott', 'WtfScott');
+bot.registry.registerGroup('rollx', 'Rollx');
+
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+bot.login("");
